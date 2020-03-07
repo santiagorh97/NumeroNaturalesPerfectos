0d24760da79716d575660c1f0aef115b0f5b3a45
luis santiago renteria hurtado <lurenteriahurta@poligran.edu.co>
2020-03-07 17:44:39 -0500

    Proyecto Inicial

--- Interfaz/Presentacion.Designer.cs
+++ Interfaz/Presentacion.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // BtnValidar
             // 
-            this.BtnValidar.Location = new System.Drawing.Point(428, 20);
+            this.BtnValidar.Location = new System.Drawing.Point(571, 25);
+            this.BtnValidar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.BtnValidar.Name = "BtnValidar";
-            this.BtnValidar.Size = new System.Drawing.Size(75, 23);
+            this.BtnValidar.Size = new System.Drawing.Size(100, 28);
             this.BtnValidar.TabIndex = 0;
             this.BtnValidar.Text = "Validar";
             this.BtnValidar.UseVisualStyleBackColor = true;
@@ -48,56 +49,62 @@
             // 
             // TbNumeroInicial
             // 
-            this.TbNumeroInicial.Location = new System.Drawing.Point(95, 22);
+            this.TbNumeroInicial.Location = new System.Drawing.Point(127, 27);
+            this.TbNumeroInicial.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TbNumeroInicial.Name = "TbNumeroInicial";
-            this.TbNumeroInicial.Size = new System.Drawing.Size(100, 20);
+            this.TbNumeroInicial.Size = new System.Drawing.Size(132, 22);
             this.TbNumeroInicial.TabIndex = 1;
             // 
             // TbNumeroFinal
             // 
-            this.TbNumeroFinal.Location = new System.Drawing.Point(279, 22);
+            this.TbNumeroFinal.Location = new System.Drawing.Point(372, 27);
+            this.TbNumeroFinal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TbNumeroFinal.Name = "TbNumeroFinal";
-            this.TbNumeroFinal.Size = new System.Drawing.Size(100, 20);
+            this.TbNumeroFinal.Size = new System.Drawing.Size(132, 22);
             this.TbNumeroFinal.TabIndex = 2;
             // 
             // TbResultado
             // 
-            this.TbResultado.Location = new System.Drawing.Point(12, 49);
+            this.TbResultado.Location = new System.Drawing.Point(16, 60);
+            this.TbResultado.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.TbResultado.Multiline = true;
             this.TbResultado.Name = "TbResultado";
-            this.TbResultado.Size = new System.Drawing.Size(491, 107);
+            this.TbResultado.Size = new System.Drawing.Size(653, 131);
             this.TbResultado.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Location = new System.Drawing.Point(16, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(101, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Numero Inicial:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(201, 25);
+            this.label2.Location = new System.Drawing.Point(268, 31);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(96, 17);
             this.label2.TabIndex = 5;
             this.label2.Text = "Numero Final:";
             // 
-            // Form1
+            // Presentacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(515, 171);
+            this.ClientSize = new System.Drawing.Size(687, 210);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.TbResultado);
             this.Controls.Add(this.TbNumeroFinal);
             this.Controls.Add(this.TbNumeroInicial);
             this.Controls.Add(this.BtnValidar);
-            this.Name = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Presentacion";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Presentacion";
             this.ResumeLayout(false);
